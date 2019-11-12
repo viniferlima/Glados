867d75decf27a7881513178abda762d838424c88
Igor <igcraval@gmail.com>
2019-11-12 20:37:18 -0300

    Bot retornando em Json mas sem dados

--- Codigo/BotFlask.py
+++ Codigo/BotFlask.py
@@ -1,190 +1,124 @@
-import requests # pip3 install requests
-import lxml # pip3 install lxml
+import requests
+import lxml
 from lxml import html
-from datetime import datetime # pip3 install datetime
-import re # 
-import schedule # pip3 install schedule
-import time #  
-import pymysql # pip3 install pymysql 
+from datetime import datetime
+import re
 from flask import Flask
 from flask import jsonify
 app = Flask(__name__)
 
-busca = "Batman" #input("Digite o nome do produto: ")
-link = ('https://www.amazon.com.br/s?k=ProdutoPesquisado&__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&ref=nb_sb_noss')
-url = link.replace("ProdutoPesquisado",busca.strip())
-errors = []
 
 
-def track(url):
+@app.route('/')
+def BotFlask():
+    
+    dados = []
+    urls = []
+    i = 0
 
-    #request xml e organização dos dados
-    res = requests.get(url, headers={'user-agent': 'glados'})
+    busca = "Eden"
+    link = ('https://www.amazon.com.br/s?k=ProdutoPesquisado&i=stripbooks&__mk_pt_BR=ÅMÅŽÕÑ&ref=nb_sb_noss_2')
+    url = link.replace("ProdutoPesquisado",busca.strip())
+
+    res = requests.get(url, headers={'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'})
+    
     doc = lxml.html.fromstring(res.content)
     corpo = doc.xpath('//*[@class="sg-row"]')[0]
+
     separador = "\n"
     link_produto = corpo.xpath('//*[@id="search"]/div[1]/div[2]/div/span[3]/div[1]/div[*]/div/span/div/div/div[2]/div[2]/div/div[1]/div/div/div[1]/h2/a/@href')
     link_site = separador.join(link_produto)
     z = link_site.split("\n")
     qtde = len(z)
-    i = 0
-    urls = []
 
-    #array de url para busca de pagina completa
     while(i < qtde):
+        res = requests.get(url, headers={'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'})
         path = str('https://www.amazon.com.br' + z[i])
         urls.append(path)
         i = 1 + i
-        #print("check while")
 
-    x = url.split(".")
-    # Trocar user-agent pelo da sua máquina (Google -> my user agent)
-    res = requests.get(url, headers={'user-agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36'})
-    while(res.status_code == 503):
-        print('Sem conexão! :c')
-    else:    
-        try:
-            #print("check try")
-            #pega todos os produtos da pagina
-            for url in urls: 
-                print(url)            
+    for url in urls:
 
-                doc = html.fromstring(res.content)
-
-                separador = ""
-
-                print("check 1")
-
-                # Site pesquisado
-                if(x[1] == "amazon"):
-                    dominio = "Amazon"
-                if(x[1] == "submarino"):
-                    dominio = "Submarino"
-                if(x[1] == "saraiva"):
-                    dominio = "Saraiva"
-                
-                print("check 2")
-                print(dominio)
-
-                # Nome do produto
-                nome_produto = doc.xpath('.//*[@id="productTitle"]/text()')
-                nome = separador.join(nome_produto).strip()
-
-                print(nome)
-                print("check 3")
-
-                # Categoria do produto
-                categoria = doc.xpath('.//*[@id="wayfinding-breadcrumbs_feature_div"]/ul/li[1]/span/a/text()')
-                cat = separador.join(categoria).strip()
-
-                print(cat)
-                print("check 4")
-
-                # Preço cheio do produto
-                fullprice = doc.xpath('.//*[@id="buyBoxInner"]/ul/li[1]/span/span[2]/text()')
-                fullprice2 = doc.xpath('.//*[@id="price"]/table/tbody/tr[1]/td[2]/span[1]/text()')
-
-                fullp = separador.join(fullprice)
-                fullp2 = separador.join(fullprice2)
-                
-                semdesc = fullp or fullp2
-
-                print(semdesc)
-                print("check 5")
-                #semdesc = separador.join(fullprice_final)
-
-                # Preço atual do produto
-                preco_produto = doc.xpath('.//*[@id="soldByThirdParty"]/span/text()')
-                preco_produto2 = doc.xpath('.//*[@id="price_inside_buybox"]/text()')
-                preco_produto3 = doc.xpath('.//*[@id="unqualifiedBuyBox"]/div/div[1]/span/text()')
-                preco_produto4 = doc.xpath('.//*[@id="priceblock_ourprice"]/text()')
-                
-                print("check 6")
-
-                preco = separador.join(preco_produto)
-                preco2 = separador.join(preco_produto2)
-                preco3 = separador.join(preco_produto3)
-                preco4 = separador.join(preco_produto4)
-
-                print("check 7")
-                
-                price = preco.strip() or preco2.strip() or preco3.strip() or preco4.strip()
-                if (semdesc > preco or semdesc > preco2 or semdesc > preco3 or semdesc > preco4):
-                    fup = semdesc.strip()
-                else:
-                    fup = preco.strip() or preco2.strip() or preco3.strip() or preco4.strip()
-                
-                print("check 8")
-                
-                price = price.replace("R$", "").replace(",", ".")
-                print(price)
-                print("checkPrecoAt1")
-                #ainda quebrando aqui, passando nulo para float(?)
-                price = float(price)
-                print("checkPrecoAt2")
-                fup = fup.replace("R$", "").replace(",",".")
-                print("checkPrecoAt3")
-                fup = float(fup)
-                print("checkPrecoAt4")
-
-                # Desconto em %
-                desconto = "%.2f" % round(((1 - (price/fup))*100),2)
-
-                print("checkDesc")
-
-                # Disponibilidade do produto
-                av = doc.xpath('.//*[@id="availability"]/span/text()')
-                disp = separador.join(av).strip()
-
-                print("checkDisp")
-
-                # Data da consulta
-                data_atual = datetime.today()
-                data_em_texto = '{}/{}/{}'.format(data_atual.day, data_atual.month, data_atual.year)
-
-                img = doc.xpath('.//*[@id="imgBlkFront"]/@data-a-dynamic-image')
-
-                print("checkData")
-
-                #checagem de dados
-                print()
-                print('Nome: ', nome)
-                print('Categoria: ', cat)
-                print("Preco atual: ", price)
-                print('Preço sem desconto: ', fup)
-                print("Porcentagem de desconto: ", desconto, "%")
-                print("Consultado em: ", data_em_texto)
-                print("Site: ", dominio)
-                print("Disponibilidade: ", disp)
-                print(img)
-
-                #parte do banco
-                connection = pymysql.connect(host="localhost", user="root", passwd="", database="glados")
-                cursor = connection.cursor()
-                insert1 = "INSERT INTO produtos(nome, categoria, preco_cheio, preco_desconto, porcentagem_desconto, site, disponibilidade, imagem) VALUES ('{}', '{}', {}, {}, {}, '{}', '{}', '{}');".format(nome, cat, fup, price, desconto, dominio, disp, img)
-                print("Código SQL para consulta: ")
-                print(insert1)
-                print()
-                cursor.execute(insert1)
-                connection.commit()
-                connection.close()
-
-            else:
-                print("Finalizado.")
-
-        except:
-            errors.append(url)
+        res = requests.get(url, headers={'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'})
+        
+        while(res.status_code == 503):
+            print('Sem conexão! :c')
             
+        else:    
+            res = requests.get(url, headers={'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'})
+            doc = html.fromstring(res.content)
+            separador = ""
+
+            # Site pesquisado
+            site = (re.findall("amazon", url) or re.findall("submarino", url) or re.findall("saraiva", url))
+            dominio = site[0].capitalize()
+            print(dominio)
+
+            # Nome do produto
+            nome_produto = doc.xpath('.//*[@id="productTitle"]/text()')
+            nome = separador.join(nome_produto).strip()
+            print(nome)
+
+            # Categoria do produto
+            categoria = doc.xpath('.//*[@id="wayfinding-breadcrumbs_feature_div"]/ul/li[1]/span/a/text()')
+            cat = separador.join(categoria).strip()
+            print(cat)
+
+            # Preço cheio do produto
+            fullprice = doc.xpath('.//*[@id="buyBoxInner"]/ul/li[1]/span/span[2]/text()')
+            semdesc = separador.join(fullprice)
+            print(semdesc)
+
+            # Preço do produto
+            preco_produto = doc.xpath('.//*[@id="soldByThirdParty"]/span/text()')
+            preco_produto2 = doc.xpath('.//*[@id="price_inside_buybox"]/text()')
+            preco_produto3 = doc.xpath('.//*[@id="unqualifiedBuyBox"]/div/div[1]/span/text()')
+            preco_produto4 = doc.xpath('.//*[@id="priceblock_ourprice"]/text()')                
+
+            preco = separador.join(preco_produto)
+            preco2 = separador.join(preco_produto2)
+            preco3 = separador.join(preco_produto3)
+            preco4 = separador.join(preco_produto4)
+
+            print(preco)
+            print(preco2)
+            print(preco3)
+            print(preco4)
+            
+            price = preco.strip() or preco2.strip() or preco3.strip() or preco4.strip()
+            if (semdesc > preco or semdesc > preco2 or semdesc > preco3 or semdesc > preco4):
+                fup = semdesc.strip()
+            else:
+                fup = preco.strip() or preco2.strip() or preco3.strip() or preco4.strip()
+
+            
+            price = price.replace("R$", "").replace(",", ".")
+            #price = float(price)
+            fup = fup.replace("R$", "").replace(",",".")
+            #fup = float(fup)
+
+            print(fup)
+
+            # Desconto em %
+            #desconto = "%.2f" % round(((1 - (price/fup))*100),2)
+            #desconto = str(desconto)
+            #desconto = desconto + "%"
+
+            # Disponibilidade do produto
+            av = doc.xpath('.//*[@id="availability"]/span/text()')
+            disp = separador.join(av).strip()
+
+            # Data da consulta
+            data_atual = datetime.today()
+            data_em_texto = '{}/{}/{}'.format(data_atual.day, data_atual.month, data_atual.year)
+
+            dado = {
+                "nome": nome,
+                "preco": price
+            }
+
+            dados.append(dado)
 
 
-
-    dados ="a"
-
-    return dados
-
-@app.route('/')
-def BotFlask():
-
-    dados = track(url)
-
+    print("Finalizado.")
     return jsonify(dados)
